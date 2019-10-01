d489dd3c1aaf6771cb4ee1700c63ddcb6a173000
Nei Thomassin Dutra <53711846+NeiTDutra@users.noreply.github.com>
2019-10-01 07:22:13 -0300

    Arquivo de rotas
    Este arquivo cria rotas desde a raiz para ser incluido no arquivo index.

---
+++ Controllers/dirdef.php
@@ -0,0 +1,9 @@
+<?php
+
+	$root = $_SERVER['DOCUMENT_ROOT'] . '/pjCELOv.1/App/';
+
+	$controllers = $root . 'Controllers/';
+	$dao = $root . 'DAO/';
+	$models = $root . 'Models/';
+	$views = $root . 'Views/';
+	$src = $root . 'src/'; 
